d0fbe2544b79daadb8b700290f75bc35b141fe93
hauszmanndavid62 <93604527+hauszmanndavid62@users.noreply.github.com>
2022-06-06 15:44:36 +0200

    Wasd

---
+++ script.js
@@ -0,0 +1,39 @@
+const gomb = document.getElementById('Kattints');
+
+
+gomb.addEventListener("click", Kattints);
+
+function Kattints() {
+  let szám = Math.floor(Math.random() * 
+  let sz = 1;
+  let tipp = prompt("Adj meg egy számot: ");
+
+  let sum = 0;
+const number = prompt('Enter a three-digit positive integer: ');
+
+
+let temp = number;
+while (temp > 0) {
+    
+    let remainder = temp % 10;
+
+    sum += remainder * remainder * remainder;
+
+    
+    temp = parseInt(temp / 10); 
+}
+
+if (sum == number) {
+    console.log(`${number} is an Armstrong number`);
+}
+else {
+    console.log(`${number} is not an Armstrong number.`);
+}
+ 
+  
+
+ 
+  tipp = prompt("Adj meg egy számot: ");
+} 
+
+  let eredmeny = document.getElementById('eredmeny');
